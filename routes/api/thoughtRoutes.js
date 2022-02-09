3be110ed5c0f3d50b250b16d099acc93abb1b306
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-08 22:21:00 -0500

    edits to thought and user routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const { put } = require('.');
+const {
+    getThoughts,
+    getSingleThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+router.route('./')
+.get(getThoughts)
+.post(createThought);
+
+router.route('./:thoughtId')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('./thoughtId/reactions')
+.post(addReaction);
+
+router.route('./thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+// api/users
+router.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+// api/users by id/
+router.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
